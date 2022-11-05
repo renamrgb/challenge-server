5106b9a17548b7bfda380fd646e25fb46337aded
Renam Bulhoes <renamgustavo@live.com>
2022-11-05 13:32:03 -0300

    Projeto Criado

---
+++ src/main/java/com/github/renamrgb/challengeserver/ChallengeServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.renamrgb.challengeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/renamrgb/challengeserver/ChallengeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.renamrgb.challengeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
